4e3cffb7678d40a556288cefa5f5e315bd857d7e
Anathanosss <anathapindika14@gmail.com>
2023-02-17 23:49:54 +0700

    seccond-commit

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['customer'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cash Register</title>
-    <link rel="icon" href="../images/<?= $logo ?>">
+    <link rel="icon" href="./assets/images/<?= $logo ?>">
     <link rel="stylesheet" href="./assets/style/style.css">
     <script src="./assets/js/jquery.js"></script>
     <script src="./assets/js/script.js"></script>
